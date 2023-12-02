80db073ac8a89bfef8afa800b5bb336395691924
nicholasmanoharan <134759240+nicholasmanoharan@users.noreply.github.com>
2023-12-01 22:27:45 -0800

    Comments

--- Item.h
+++ Item.h
@@ -5,13 +5,17 @@
 
 class Item {
 public:
+    // Constructor to initialize the item with a name
     Item(const std::string& name);
+
+    // Destructor to clean up resources (not needed for this simple class)
     ~Item();
 
+    // Getter function to retrieve the item's name
     std::string getName() const;
 
 private:
-    std::string name;
+    std::string name;  // Member variable to store the item's name
 };
 
-#endif 
+#endif
