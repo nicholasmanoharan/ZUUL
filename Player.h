d493e4022c74504df4bd997cce016f0a8ffbc384
nicholasmanoharan <134759240+nicholasmanoharan@users.noreply.github.com>
2023-12-01 22:28:47 -0800

    comments

--- Player.h
+++ Player.h
@@ -1,19 +1,28 @@
 #ifndef PLAYER_H
 #define PLAYER_H
+
 #include <vector>
 #include "Item.h"
 
 class Player {
 public:
+    //Initialize the Player's inventory
     Player();
+
+    
     ~Player();
 
+    //Adding an item 
     void addItem(Item* item);
+
+    //Removing an item
     void removeItem(Item* item);
+
+    //Get current inventory
     std::vector<Item*> getInventory() const;
 
 private:
-    std::vector<Item*> inventory;
+    std::vector<Item*> inventory;  // Member variable to store the player's inventory
 };
 
-#endif 
+#endif
